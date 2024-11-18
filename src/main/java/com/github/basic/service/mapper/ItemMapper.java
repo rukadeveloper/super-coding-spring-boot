109c7924d5af47fc4cfa2cb622183a4a3ad2714e
rukadeveloper <87594020+rukadeveloper@users.noreply.github.com>
2024-11-18 22:35:16 +0900

    add

--- src/main/java/com/github/basic/service/mapper/ItemMapper.java
+++ src/main/java/com/github/basic/service/mapper/ItemMapper.java
@@ -2,6 +2,7 @@ package com.github.basic.service.mapper;
 
 import com.github.basic.repository.item.ItemEntity;
 import com.github.basic.web.dto.Item;
+import com.github.basic.web.dto.ItemBody;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -16,4 +17,10 @@ public interface ItemMapper {
     @Mapping(target = "spec.cpu", source = "cpu")
     @Mapping(target = "spec.capacity", source = "capacity")
     Item itemEntityToItem(ItemEntity itemEntity);
+
+    @Mapping(target="cpu", source="itemBody.spec.cpu")
+    @Mapping(target = "capacity", source = "itemBody.spec.capacity")
+    @Mapping(target = "storeId", ignore = true)
+    @Mapping(target = "stock", expression = "java(0)")
+    ItemEntity itemBodyToItemEntity(ItemBody itemBody);
 }
